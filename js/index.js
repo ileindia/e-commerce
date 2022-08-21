6be685b40fc393236a7b13120312ffc1014b22b5
ileindia <ile.irigoite20@hotmail.com>
2022-08-20 21:11:19 -0300

    cambio

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ regBtn.addEventListener("click", () => {
     let inputs = [password.value, email.value];
 
     if (inputs.every(notEmpty)){
-        windows.location.href="frontpage.html";
+        window.location.href="frontpage.html";
     } else {
         alert("Complete los campos");
     }
